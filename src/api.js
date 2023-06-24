6de4963cf6a4f53f674bc31ced20884cf5454438
lokesh-1509 <lokeshreddyvelpula03@gmail.com>
2023-06-25 01:40:28 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '55c836af72msh8a4d282df377fa2p1a1701jsn7b67dbdf47a6',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY="08e74ec0a4c1d33d4ee5da292ed0f069";
+
+
